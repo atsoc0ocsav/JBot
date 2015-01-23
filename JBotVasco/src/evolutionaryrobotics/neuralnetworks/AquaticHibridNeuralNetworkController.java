c33c93e5ebd98f9cf38138a269b2d7817c613539
atsoc0ocsav@gmail.com <atsoc0ocsav@gmail.com@2322f118-435d-c43a-7cd3-233c37dbdc83>
2015-01-23 17:00:23 +0000

    Many changes on motorsvelocitysensor, faultytwowheelactuator, mymaritimemissionenvironment, reorganization of controller packages, and on the fitness function (faultdetectionevaluationfuntion)

--- JBotVasco/src/evolutionaryrobotics/neuralnetworks/AquaticHibridNeuralNetworkController.java
+++ JBotVasco/src/evolutionaryrobotics/neuralnetworks/AquaticHibridNeuralNetworkController.java
@@ -4,21 +4,22 @@ import simulation.Simulator;
 import simulation.robot.Robot;
 import simulation.util.Arguments;
 import simulation.util.ArgumentsAnnotation;
-import controllers.PassByWaypointsController;
 
-public class AquaticHibridNeuralNetworkController extends NeuralNetworkController {
+public class AquaticHibridNeuralNetworkController extends
+		NeuralNetworkController {
 	protected PassByWaypointsController passByWPController;
-	
-	@ArgumentsAnnotation(name="printweights", values={"0","1"})
+
+	@ArgumentsAnnotation(name = "printweights", values = { "0", "1" })
 	protected boolean printWeights = false;
 
-	public AquaticHibridNeuralNetworkController(Simulator simulator, Robot robot, Arguments args) {
+	public AquaticHibridNeuralNetworkController(Simulator simulator,
+			Robot robot, Arguments args) {
 		super(simulator, robot, args);
-		
-		passByWPController = new PassByWaypointsController(simulator, robot, args);
+
+		passByWPController = new PassByWaypointsController(simulator, robot,
+				args);
 	}
 
-	
 	@Override
 	public void controlStep(double time) {
 		super.controlStep(time);
